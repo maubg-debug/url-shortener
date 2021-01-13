b810c201dd0d7db4d3e35f5b84dd2632021b8673
maubg-debug <maubg2007@gmail.com>
2021-01-13 08:35:12 +0100

    awd

--- main.go
+++ main.go
@@ -8,54 +8,56 @@ import (
 	"net/http"
 )
 
-type Respuesta struct {
-	status bool   `json:"estado"`
-	codigo string `json:"codigo"`
-}
-
-func fetch(url string) {
-
-	urlN := "https://maubot.maucode.com/api/redir/crear?url=" + url
-
-	req, err := http.NewRequest("GET", urlN, nil)
-	if err != nil {
-		log.Fatal("NewRequest: ", err)
-		return
-	}
-
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Do: ", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	var record Respuesta
-
-	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
-		log.Println(err)
-	}
-
-	return record.codigo
-}
-
 func main() {
 
 	tmpl := template.Must(template.ParseFiles("web/index.html"))
-	// fs := http.FileServer(http.Dir("./assets"))
+	fs := http.FileServer(http.Dir("./assets"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
+			tmpl.Execute(w, fs)
 			return
+		}
+
+		type Respuesta struct {
+			codigo string `json:"codigo"`
+			estado int    `json:"estado"`
 		}
 
 		data := r.FormValue("url")
 
-		codigo := fetch(data)
+		urlN := "https://maubot.maucode.com/api/redir/crear?url=" + data
+
+		req, err := http.NewRequest("GET", urlN, nil)
+		if err != nil {
+			log.Fatal("NewRequest: ", err)
+			return
+		}
+
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal("Do: ", err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		var record Respuesta
+
+		if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+			log.Println(err)
+			return
+		}
+
+		codigo := ""
+
+		if record.estado == 200 {
+			codigo = "https://maubot.maucode.com/api/redir?codigo=" + record.codigo
+		} else {
+			codigo = ""
+		}
 
 		fmt.Printf(codigo)
 	})
